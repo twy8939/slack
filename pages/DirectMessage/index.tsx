d7b0235756395e438f9a7911b566d765929c73ab
twy8939 <twy8939@naver.com>
2024-02-23 23:31:22 +0900

    feat : 채팅 보내기 추가

--- pages/DirectMessage/index.tsx
+++ pages/DirectMessage/index.tsx
@@ -7,21 +7,38 @@ import { useParams } from 'react-router';
 import ChatBox from '@components/ChatBox';
 import ChatList from '@components/ChatList';
 import { useInput } from '@hooks/useInput';
+import axios from 'axios';
+import { IDM } from '@typings/db';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const { data: myData } = useSWR('/api/users', fetcher);
   const [chat, onChangeChat, setChat] = useInput('');
+  const { data: chatData, mutate: mutateChat } = useSWR<IDM[]>(
+    `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+    fetcher,
+  );
 
   const onSubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log('submit');
+    if (chat?.trim()) {
+      console.log(chat, 'chat');
 
-    setChat('');
+      axios
+        .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+          content: chat,
+        })
+        .then(() => {
+          mutateChat();
+          setChat('');
+        })
+        .catch(console.error);
+    }
   };
 
   if (!userData || !myData) return null;
+
   return (
     <Container>
       <Header>
