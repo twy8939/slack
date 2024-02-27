bc1b991b6c679ef842b34a5e42824b4ee685b96a
twy8939 <twy8939@naver.com>
2024-02-27 23:20:32 +0900

    feat : dayjs 추가

--- components/ChatList/index.tsx
+++ components/ChatList/index.tsx
@@ -2,7 +2,7 @@ import React, { VFC, useRef } from 'react';
 import { ChatZone, Section } from './styles';
 import { IDM } from '@typings/db';
 import Chat from '@components/Chat';
-import { Scrollbars } from 'react-custom-scrollbars';
+import Scrollbars from 'react-custom-scrollbars-2';
 
 interface Props {
   chatData?: IDM[];
