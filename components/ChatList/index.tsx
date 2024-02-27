6851adcf69f32b6f26dc887c4ce2ace5784be6b1
twy8939 <twy8939@naver.com>
2024-02-27 23:08:33 +0900

    feat : react-custom-scrollbars 추가

--- components/ChatList/index.tsx
+++ components/ChatList/index.tsx
@@ -1,18 +1,24 @@
-import React, { VFC } from 'react';
+import React, { VFC, useRef } from 'react';
 import { ChatZone, Section } from './styles';
 import { IDM } from '@typings/db';
 import Chat from '@components/Chat';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
   chatData?: IDM[];
 }
 
 const ChatList: VFC<Props> = ({ chatData }) => {
+  const scrollbarRef = useRef(null);
+  const onScroll = () => {};
+
   return (
     <ChatZone>
-      {chatData?.map((chat) => (
-        <Chat key={chat.id} data={chat} />
-      ))}
+      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+        {chatData?.map((chat) => (
+          <Chat key={chat.id} data={chat} />
+        ))}
+      </Scrollbars>
     </ChatZone>
   );
 };
