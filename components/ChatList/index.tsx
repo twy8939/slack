ecf8277f5307552e681ee09e2ae704f9023f1378
twy8939 <twy8939@naver.com>
2024-03-01 20:03:24 +0900

    feat : 채팅 날짜 구분 추가

--- components/ChatList/index.tsx
+++ components/ChatList/index.tsx
@@ -1,22 +1,29 @@
 import React, { VFC, useRef } from 'react';
-import { ChatZone, Section } from './styles';
+import { ChatZone, Section, StickyHeader } from './styles';
 import { IDM } from '@typings/db';
 import Chat from '@components/Chat';
 import Scrollbars from 'react-custom-scrollbars-2';
 
 interface Props {
-  chatData?: IDM[];
+  chatSections: { [KEY: string]: IDM[] };
 }
 
-const ChatList: VFC<Props> = ({ chatData }) => {
+const ChatList: VFC<Props> = ({ chatSections }) => {
   const scrollbarRef = useRef(null);
   const onScroll = () => {};
 
   return (
     <ChatZone>
       <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
-        {chatData?.map((chat) => (
-          <Chat key={chat.id} data={chat} />
+        {Object.entries(chatSections).map(([date, chats]) => (
+          <Section className={`section-${date}`} key={date}>
+            <StickyHeader>
+              <button>{date}</button>
+            </StickyHeader>
+            {chats.map((chat) => (
+              <Chat key={chat.id} data={chat} />
+            ))}
+          </Section>
         ))}
       </Scrollbars>
     </ChatZone>
