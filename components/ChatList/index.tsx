f36b9d76e449571e84ed757f9b21c56445e9638e
twy8939 <twy8939@naver.com>
2024-03-03 16:54:46 +0900

    feat : 채팅방 > 스크롤 유지 기능 추가

--- components/ChatList/index.tsx
+++ components/ChatList/index.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react';
+import React, { MutableRefObject, forwardRef } from 'react';
 import { ChatZone, Section, StickyHeader } from './styles';
 import { IDM } from '@typings/db';
 import Chat from '@components/Chat';
@@ -7,16 +7,18 @@ import Scrollbars, { positionValues } from 'react-custom-scrollbars-2';
 interface Props {
   chatSections: { [KEY: string]: IDM[] };
   setSize: (f: (indeX: number) => number) => Promise<IDM[][] | undefined>;
-  isEmpty: boolean;
   isReachingEnd: boolean;
 }
 
-const ChatList = forwardRef<Scrollbars, Props>(({ chatSections, setSize, isEmpty, isReachingEnd }, ref) => {
+const ChatList = forwardRef<Scrollbars, Props>(({ chatSections, setSize, isReachingEnd }, ref) => {
   const onScroll = (values: positionValues) => {
+    const current = (ref as MutableRefObject<Scrollbars>)?.current;
     if (values.scrollTop === 0 && !isReachingEnd) {
-      console.log('위');
       setSize((prevSize) => prevSize + 1).then(() => {
         // 스크롤 위치 유지
+        if (current) {
+          current.scrollTop(1);
+        }
       });
     }
   };
